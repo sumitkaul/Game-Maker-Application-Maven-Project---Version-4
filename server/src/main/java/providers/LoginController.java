4f1b62bfc102e26a1f269e293f4c530966a52367
Karthik Nagaraj <kanmalle@indiana.edu>
2012-11-11 23:49:09 -0500

    http://tintin.cs.indiana.edu:8091/browse/FATWVTEN-31
    Registering user using Spring MVC.

--- server/src/main/java/providers/LoginController.java
+++ server/src/main/java/providers/LoginController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -23,30 +24,51 @@ import db.User;
 @Controller
 public class LoginController {
 
-	@RequestMapping(value="/loginUser", method = RequestMethod.GET)
+	@RequestMapping(value = "/loginUser", method = RequestMethod.GET)
 	@ResponseBody
-	public ResponseEntity<Gson> login(HttpEntity<byte[]> requestEntity)
-	{
-		
-		  Gson gson = new Gson();
-		  HttpHeaders requestHeader=requestEntity.getHeaders();
-		  String userName=requestHeader.get("username").get(0);
-		  String passWord=requestHeader.get("password").get(0);
-		  
-		  Session session = DatabaseHandler.getDatabaseHandlerInstance().getHibernateSession();
-          Criteria criteria = session.createCriteria(User.class);
-          criteria.add(Restrictions.eq("username", userName));
-          criteria.add(Restrictions.eq("password", passWord));
-          List r = criteria.list();
-          if (r.isEmpty()) {
-        	  gson.toJson(false);
-              
-          } else {
-              gson.toJson(true);
-              
-          }
-          session.close();
-          return new ResponseEntity<Gson>(gson,null);
-		
+	public ResponseEntity<Gson> login(HttpEntity<byte[]> requestEntity) {
+
+		Gson gson = new Gson();
+		HttpHeaders requestHeader = requestEntity.getHeaders();
+		String userName = requestHeader.get("username").get(0);
+		String passWord = requestHeader.get("password").get(0);
+
+		Session session = DatabaseHandler.getDatabaseHandlerInstance()
+				.getHibernateSession();
+		Criteria criteria = session.createCriteria(User.class);
+		criteria.add(Restrictions.eq("username", userName));
+		criteria.add(Restrictions.eq("password", passWord));
+		List r = criteria.list();
+		if (r.isEmpty()) {
+			gson.toJson(false);
+
+		} else {
+			gson.toJson(true);
+
+		}
+		session.close();
+		return new ResponseEntity<Gson>(gson, null);
+
+	}
+
+	@RequestMapping(value = "/registerUser", method = RequestMethod.GET)
+	@ResponseBody
+	public ResponseEntity<Gson> register(HttpEntity<byte[]> requestEntity) {
+		Gson gson = new Gson();
+		HttpHeaders requestHeader = requestEntity.getHeaders();
+		String userName = requestHeader.get("username").get(0);
+		String passWord = requestHeader.get("password").get(0);
+
+		Session session = DatabaseHandler.getDatabaseHandlerInstance()
+				.getHibernateSession();
+		Transaction t = session.beginTransaction();
+		User u = new User(userName, passWord, "");
+		session.save(u);
+		t.commit();
+
+		session.close();
+		gson.toJson(true);
+		return new ResponseEntity<Gson>(gson, null);
+
 	}
 }
