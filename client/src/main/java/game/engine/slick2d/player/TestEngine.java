7516017767139450174238edf834a104401a4fc4
minglu <minglu@indiana.edu>
2012-11-14 22:15:06 -0500

    Temporarily add start/stop functions to launch GameEngine Frame from OptionFrame for testing http://tintin.cs.indiana.edu:8091/browse/FATWVTEN-41

--- client/src/main/java/game/engine/slick2d/player/TestEngine.java
+++ client/src/main/java/game/engine/slick2d/player/TestEngine.java
@@ -1,25 +1,48 @@
 package game.engine.slick2d.player;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.SlickException;
 
 public class TestEngine {
-
-    public static void main(String[] args) throws SlickException, IOException {
+    
+    private static final org.apache.log4j.Logger LOG = 
+			org.apache.log4j.Logger.getLogger(TestEngine.class);
+    private AppGameContainer app;
+    
+    public TestEngine(AppGameContainer app){
+        
+        LOG.debug("This is Game Engine Test");
         System.setProperty("org.lwjgl.librarypath", System.getProperty("user.dir") + "/target/natives/");
         System.setProperty("net.java.games.input.librarypath", System.getProperty("org.lwjgl.librarypath"));
 
-        GameEngineController game = new GameEngineController("test", GameEngineController.LOAD_MODE_REMOTE, null);
+        //GameEngineController game = new GameEngineController("test", GameEngineController.LOAD_MODE_REMOTE, null);
+        this.app = app;
 
+        try {
+            //app = new AppGameContainer(game);
+            app.setTargetFrameRate(100);
+            app.setDisplayMode(800, 600, false);
+            app.setAlwaysRender(true);
+            app.setForceExit(false);
+            //app.setClearEachFrame(true);
+        
 
-        AppGameContainer app = new AppGameContainer(game);
-        app.setTargetFrameRate(100);
-        app.setDisplayMode(800, 600, false);
-        app.setAlwaysRender(true);
-        //app.setClearEachFrame(true);
-
-
-        app.start();
+            //app.start();
+        } catch (SlickException ex) {
+            Logger.getLogger(TestEngine.class.getName()).log(Level.SEVERE, null, ex);
+        }
+ 
     }
+
+    public AppGameContainer getApp() {
+        return app;
+    }
+
+    public void setApp(AppGameContainer app) {
+        this.app = app;
+    }
+    
 }
