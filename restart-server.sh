b5cc723946427144be4c92bab5364571b39f4c50
Viknes <Viknes@192.168.2.4>
2012-11-20 18:21:14 -0500

    Dummy commit to trigger the build. Just provided execute permissions for tmp folder.

--- restart-server.sh
+++ restart-server.sh
@@ -9,8 +9,6 @@ then
 fi
 
 
-
-
 /u/kspace/local/maven/bin/mvn -f /u/kspace/www/fluency/student-files/fall2012/a10/team-all/server/pom.xml clean install > $LOGFILE 2>&1
 /u/kspace/local/maven/bin/mvn -f /u/kspace/www/fluency/student-files/fall2012/a10/team-all/server/pom.xml cargo:run >> $LOGFILE 2>&1 &
 pid=$!
