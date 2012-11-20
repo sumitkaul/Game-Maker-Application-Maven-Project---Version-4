ae0319fdc266ee1d84953f189c6c85fb5cb3509e
Hari Prasad <hhariprsd@gmail.com>
2012-11-20 11:05:58 -0500

    php script for Facebook login authentication ( http://tintin.cs.indiana.edu:8091/browse/FATWVTEN-64 )

--- server/fb_login.php
+++ server/fb_login.php
@@ -18,11 +18,25 @@
    }
    
    if($_SESSION['state'] && ($_SESSION['state'] === $_REQUEST['state'])) {
-     echo("<script> top.location.href='" . urlencode($my_url) . "state="
-       . $_SESSION['state'] . "&code=" . $code . "'</script>");
+     $token_url = "https://graph.facebook.com/oauth/access_token?"
+       . "client_id=" . $app_id . "&redirect_uri=" . urlencode($my_url)
+       . "&client_secret=" . $app_secret . "&code=" . $code;
+
+     $response = file_get_contents($token_url);
+     $params = null;
+     parse_str($response, $params);
+
+     $_SESSION['access_token'] = $params['access_token'];
+
+     $graph_url = "https://graph.facebook.com/me?access_token=" 
+       . $params['access_token'];
+
+     $user = json_decode(file_get_contents($graph_url));
+     echo("Hello " . $user->name);
    }
    else {
      echo("The state does not match. You may be a victim of CSRF.");
    }
    
  ?>
+ 
