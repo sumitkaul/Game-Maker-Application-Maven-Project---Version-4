fd2579bb3097aee6151782de29517dd512554aab
mjaffee <mjaffee@indiana.edu>
2012-11-20 15:55:03 -0500

    adding mvn install before cargo:run to the restart-server script

--- restart-server.sh
+++ restart-server.sh
@@ -9,6 +9,9 @@ then
 fi
 
 
-/u/kspace/local/maven/bin/mvn -f /u/kspace/www/fluency/student-files/fall2012/a10/team-all/server/pom.xml cargo:run > $LOGFILE 2>&1 &
+
+
+/u/kspace/local/maven/bin/mvn -f /u/kspace/www/fluency/student-files/fall2012/a10/team-all/server/pom.xml install > $LOGFILE 2>&1 &
+/u/kspace/local/maven/bin/mvn -f /u/kspace/www/fluency/student-files/fall2012/a10/team-all/server/pom.xml cargo:run >> $LOGFILE 2>&1 &
 pid=$!
 echo $pid > $PIDFILE
