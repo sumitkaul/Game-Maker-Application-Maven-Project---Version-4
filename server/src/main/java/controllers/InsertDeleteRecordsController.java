ac1b30250cd930168d0fa6c67b34e5df307875a7
Tim Gilmanov <timugilm@umail.iu.edu>
2012-11-20 02:09:31 -0500

    --> Adding getHostedGameBaseId mapping --> JIRA issue: http://tintin.cs.indiana.edu:8091/browse/FATWVTEN-48

--- server/src/main/java/controllers/InsertDeleteRecordsController.java
+++ server/src/main/java/controllers/InsertDeleteRecordsController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package controllers;
 
 import java.math.BigInteger;
@@ -56,66 +55,3 @@ public class InsertDeleteRecordsController {
 	}
 
 }
-=======
-package controllers;
-
-import java.math.BigInteger;
-import java.util.List;
-
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.jboss.logging.Logger;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.google.gson.Gson;
-
-import db.DatabaseHandler;
-import db.Resources;
-
-@Controller
-public class InsertDeleteRecordsController {
-
-	private static Logger log = Logger.getLogger(InsertDeleteRecordsController.class);
-	
-	@RequestMapping(value = "/deleteHostedGameBaseRecord", method = RequestMethod.POST)
-	@ResponseBody
-	public ResponseEntity<Gson> saveGame(HttpEntity<byte[]> requestEntity) {
-		
-		HttpHeaders requestHeader = requestEntity.getHeaders();
-		
-		String gameId = requestHeader.get("id").get(0);
-
-        Gson gson = new Gson();
-
-        if (gameId != null) {
-
-            String sql = "select count(*) FROM HostedGameBases where id=" + gameId;
-            String sql1 = "delete from HostedGameBases where id="+gameId;
-
-            List<BigInteger> count = DatabaseHandler.Query(sql);
-            log.info(count);
-
-            if (count.get(0).intValue() < 1) {
-            	//let the caller know that there was no record with a requested id
-            } else {
-                DatabaseHandler.ExecuteQuery(sql1);
-            }
-
-            log.info("deleted entry id: " + gameId);
-
-            gson.toJson(true);
-        } else {
-            gson.toJson(false);
-        }
-        
-        return new ResponseEntity<Gson>(gson, null);
-	}
-	
-}
->>>>>>> 3fa69d26204b71b8589b53f8c057740d8ccd2ae2
