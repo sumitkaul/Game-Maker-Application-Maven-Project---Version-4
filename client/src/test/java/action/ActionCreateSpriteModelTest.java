1f1cd4fef2f05d663b09b0417507e77077e5f677
Pavan Venkatramanachar <pkvenkat@indiana.edu>
2012-11-21 23:36:24 -0500

    Adding unittests for couple of Actions -
    http://tintin.cs.indiana.edu:8091/browse/FATWVTEN-51

---
+++ client/src/test/java/action/ActionCreateSpriteModelTest.java
@@ -0,0 +1,66 @@
+package action;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import model.SpriteModel;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import eventlistener.EventListener;
+import eventlistener.NewFrameEventListener;
+
+import utility.ResizeHelper;
+import utility.SpriteList;
+import view.GameMakerView;
+
+public class ActionCreateSpriteModelTest {
+	
+	@Before
+	public void setUp() throws Exception {
+		GameMakerView.getInstance().reset();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		GameMakerView.getInstance().reset();
+	}
+
+	@Test
+	public void testDoAction() {
+		
+	    SpriteModel	selectedSpriteModel = new SpriteModel(190, 190, 10, 10, 100, 100, "","");
+      	selectedSpriteModel.setGroupId("Group1");
+      	GameAction action = new ActionCreateSpriteModel();
+        action.doAction(selectedSpriteModel);
+		List<SpriteModel> allSpriteList = SpriteList.getInstance().getSpriteList();
+		SpriteModel newModel =null;
+		
+		for(SpriteModel sm : allSpriteList){
+			if(sm.getGroupId().equalsIgnoreCase("Bomb"))
+			{
+				newModel =  sm;
+				break;
+			}	
+       }
+	 if (newModel == null)
+			assertTrue(false);
+	 
+	 boolean foundBombMoveListener = false;
+	 List<EventListener> listenerList = GameMakerView.getInstance().getFacade().getGameController().getEvents();
+		for(EventListener listener : listenerList){
+			if(listener instanceof NewFrameEventListener){
+				NewFrameEventListener newFrameListerner = (NewFrameEventListener)listener;
+				String groupId = newFrameListerner.getRegisteredGroupId();
+				if(groupId.equalsIgnoreCase("Bomb"))
+					foundBombMoveListener = true;
+			   } 
+
+        }
+		assertTrue(foundBombMoveListener);			
+		
+	}	
+}
