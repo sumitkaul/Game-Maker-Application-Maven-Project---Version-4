92b5c3731774949460ef29ff84d9f1699a72e66a
asnair <asnair@indiana.edu>
2012-11-22 17:08:45 -0500

    Added tests with respect to the JIRA issue : http://tintin.cs.indiana.edu:8091/browse/FATWVTEN-109#comment-10610
    Added tests for GameBase and GameProgressSaveInfo classes and cleaned up
    commented code in "Resource.java". This class may be removed by tonight
    as it seems unused as code from this class has been shifted to
    "Resources.java". Holding up on the team's response before refactoring
    and removing the unused class.

--- client/src/main/java/model/Resource.java
+++ client/src/main/java/model/Resource.java
@@ -26,7 +26,6 @@ public class Resource {
 	public byte[] getData() {
 		return data;
 	}
-	
 	 
 	public void setName(String name) {
 		this.name = name;
@@ -42,42 +41,5 @@ public class Resource {
 	}
 	public void setUsername(String username) {
 		this.username = username;
-	}
-	
-	
-//	 /** Don't invoke this.  Used by Hibernate only. */ 
-//	  public void setDataBlob(Blob blob) {  
-//	   this.data = this.toByteArray(blob); 
-//	  } 
-//	  
-//	  /** Don't invoke this.  Used by Hibernate only. */ 
-//	  public Blob getDataBlob() {  
-//	   return  Hibernate.getLobCreator(HibernateUtil.getSessionFactory().getCurrentSession()).createBlob(this.data); 
-//	  } 
-//	  
-//	  private byte[] toByteArray(Blob fromBlob) {  
-//	   ByteArrayOutputStream baos = new ByteArrayOutputStream();  
-//	   try {   
-//	    return toByteArrayImpl(fromBlob, baos);  
-//	   } catch (SQLException e) {   
-//	    throw new RuntimeException(e); 
-//	   } catch (IOException e) {   
-//	    throw new RuntimeException(e);  
-//	   } finally {   
-//	    if (baos != null) {  
-//	     try {   
-//	      baos.close(); 
-//	     } catch (IOException ex) {    }   }  } } 
-	  
-//	  private byte[] toByteArrayImpl(Blob fromBlob, ByteArrayOutputStream baos)  
-//	  throws SQLException, IOException {  
-//	   byte[] buf = new byte[4000];  
-//	   InputStream is = fromBlob.getBinaryStream();  
-//	   try {   for (;;) {    
-//	    int dataSize = is.read(buf);   
-//	    if (dataSize == -1)     break;   
-//	    baos.write(buf, 0, dataSize);   }  } finally {   if (is != null) { 
-//	     try {     is.close();    } catch (IOException ex) {    }   }  }  
-//	    return baos.toByteArray(); }
-	  
-	 }
+	}  
+ }
