df476fdcfa2309e8c4fbfbe41bbd94f7b44c2b0a
sjeganat <sjeganat@indiana.edu>
2012-11-25 14:50:59 -0500

    Receiver class for Chat JIRA: http://tintin.cs.indiana.edu:8091/browse/FATWVTEN-22

---
+++ server/src/main/java/chat/ChatUsersReceiver.java
@@ -0,0 +1,85 @@
+package chat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jms.Connection;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+
+public class ChatUsersReceiver implements Runnable {
+	
+	private List<String> activeUsers;
+	private MessageConsumer consumer;
+	private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(ChatUsersReceiver.class);
+	private static final String ActiveMQConnect = "tcp://129.79.247.5:61616";
+	
+	public ChatUsersReceiver(){
+		activeUsers = new ArrayList<String>();
+		try {
+			// Create a ConnectionFactory
+			ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(ActiveMQConnect);
+
+			// Create a Connection
+			Connection connection = connectionFactory.createConnection();
+			connection.start();
+
+
+			// connection.setExceptionListener((ExceptionListener) this);
+
+			// Create a Session
+			Session session = connection.createSession(false,
+					Session.AUTO_ACKNOWLEDGE);
+
+			// Create the destination (Topic or Queue)
+			//Destination destination = session.createQueue("CHAT");
+			Topic topic= session.createTopic("BROADCAST");
+			// Create a MessageConsumer from the Session to the Topic or Queue
+			consumer = session.createConsumer(topic);
+			
+		} catch (Exception ex) {
+			LOG.error(ex.getMessage());
+		}
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				// Wait for a message
+				Message message = consumer.receive();
+				if (message instanceof TextMessage) {
+					TextMessage textMessage = (TextMessage) message;
+					String text = textMessage.getText();
+					//when the user replies back the message should be in the format "Alive:Username:Game"
+					if(text.startsWith("Alive")){
+						String requiredText = text.substring(text.indexOf(':'));
+						activeUsers.add(requiredText);
+					}
+				}
+			} catch (Exception e) {
+				LOG.error(e.getMessage());
+			}
+		}
+		
+
+	}
+
+
+	public List<String> getActiveUsers() {
+		return activeUsers;
+	}
+	
+	public void clearActiveUsers(){
+		activeUsers.clear();
+	}
+	
+	
+
+}
