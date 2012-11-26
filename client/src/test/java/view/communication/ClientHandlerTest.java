d9874de73e55f37e04bbb6b49c0385dc84fc0d76
harsh <harsh.savla@gmail.com>
2012-11-26 14:08:34 -0500

    http://tintin.cs.indiana.edu:8091/browse/FATWVTEN-109 Adding  test for ClientHandler method "loadGameBase" and some code cleanup

--- client/src/test/java/view/communication/ClientHandlerTest.java
+++ client/src/test/java/view/communication/ClientHandlerTest.java
@@ -1,32 +1,29 @@
 package view.communication;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 
-import org.apache.http.client.utils.URIBuilder;
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
 import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import org.powermock.api.mockito.PowerMockito;
-import org.mockito.Matchers;
-import com.google.gson.Gson;
- 
-
 
 import view.utils.HttpUtil;
+
+import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
 
 @RunWith( PowerMockRunner.class )
 @PrepareForTest( HttpUtil.class )
 
 public class ClientHandlerTest {
-    private final static Logger log = Logger.getLogger(ClientHandlerTest.class);
+	private final static Logger log = Logger.getLogger(ClientHandlerTest.class);
 
 	@Before
 	public void setUp() throws Exception {
@@ -39,31 +36,42 @@ public class ClientHandlerTest {
 	@Test
 	public void testListAllGameBases() {
 		PowerMockito.mockStatic(HttpUtil.class);
-		
-		   //URIBuilder ub = PowerMockito.mock(URIBuilder.class);  -- For Ref 
-		  
-		  try {
-			//PowerMockito.whenNew(URIBuilder.class).withNoArguments().thenReturn(ub);			
+
+		try {
 			String[] values = {"Game1", "Game2","Game3", "Game4"};
 			Gson g = new Gson();
 			String gameListJson = g.toJson(values);				
 			PowerMockito.when(HttpUtil.class, HttpUtil.httpGet((URI)(Matchers.any()))).thenReturn(gameListJson);
-			
+
 			String[] value = ClientHandler.listAllGameBases("testserver:1000", "/listGames", new Exception[1]);
 			log.debug("first game name="+value[0]);
 			assertEquals(value[0], "Game1");
 			assertEquals(value[1], "Game2");
 			assertEquals(value[2], "Game3");
-			
+
 		} catch (Exception e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		  
-		 // PowerMockito.  expect(HttpUtil.httpGet(ub.build())
-				
 	}
 
-	
+	/* mocking the httpget method from the HttpUtil class */
+	@Test
+	public void testLoadGameBase() {
 
+		PowerMockito.mockStatic(HttpUtil.class);
+
+		try {
+
+			String gameData = "This is the game data being mocked";
+			XStream xStream = new XStream();
+			String gameDataXML = xStream.toXML(gameData);
+			PowerMockito.when(HttpUtil.class, HttpUtil.httpGet((URI) (Matchers.any()))).thenReturn(gameDataXML);
+
+			String actualResult = ClientHandler.loadGameBase("testgame", "testhost:8080", "/loadGameBase",
+					new Exception[1]);
+			assertEquals(gameDataXML, actualResult);
+		} catch (Exception e) {
+			log.debug("Test failed");
+		}
+	}
 }
