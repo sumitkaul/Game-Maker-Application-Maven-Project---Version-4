5a97ba9b9d039b88cabc77e7650e0329b3ad0586
Mayur Bharat Masrani <mmasrani@silo.cs.indiana.edu>
2012-11-28 03:08:13 -0500

    Adding Facebook Authentication and post message on facebook scripts http://tintin.cs.indiana.edu:8091/browse/FATWVTEN-64

---
+++ server/facebook/fbdata/fbmain.php
@@ -0,0 +1,10 @@
+<?php
+require 'include/src/facebook.php';
+$facebook = new Facebook(array(
+			'appId'  => '385974881489282',
+			'secret' => '6f4383773dd552ea9f149892684c13c7',
+			'cookie' => false,
+
+			));
+
+?>
