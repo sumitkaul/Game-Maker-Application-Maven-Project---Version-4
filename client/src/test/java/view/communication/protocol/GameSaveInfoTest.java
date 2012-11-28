9ac04a1851bd15e5434009ccddb14d5e6b0dcbc4
Ritesh <Ritesh@10.0.0.3>
2012-11-28 13:45:38 -0500

    Added test for GameSaveInfo on Client

---
+++ client/src/test/java/view/communication/protocol/GameSaveInfoTest.java
@@ -0,0 +1,175 @@
+package view.communication.protocol;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class GameSaveInfoTest {
+	/**
+	 * Run the GameSaveInfo(String,String,String,int) constructor test.
+	 *
+	 * @throws Exception
+	 */
+	@Test
+	public void testGameSaveInfo_1()
+		throws Exception {
+		String saveName = "";
+		String gameName = "";
+		String gamePlayer = "";
+		int id = 1;
+
+		GameSaveInfo result = new GameSaveInfo(saveName, gameName, gamePlayer, id);
+
+		assertNotNull(result);
+		assertEquals(1, result.getId());
+		assertEquals(0, result.getRank());
+		assertEquals(0, result.getGameScore());
+		assertEquals("", result.getSaveName());
+		assertEquals("", result.getGameName());
+		assertEquals(null, result.getGameData());
+		assertEquals("", result.getGamePlayer());
+	}
+
+	/**
+	 * Run the GameSaveInfo(String,String,String,String) constructor test.
+	 *
+	 * @throws Exception
+	 */
+	@Test
+	public void testGameSaveInfo_2()
+		throws Exception {
+		String saveName = "";
+		String gameName = "";
+		String gamePlayer = "";
+		String gameData = "";
+
+		GameSaveInfo result = new GameSaveInfo(saveName, gameName, gamePlayer, gameData);
+
+		assertNotNull(result);
+		assertEquals(0, result.getId());
+		assertEquals(0, result.getRank());
+		assertEquals(0, result.getGameScore());
+		assertEquals("", result.getSaveName());
+		assertEquals("", result.getGameName());
+		assertEquals("", result.getGameData());
+		assertEquals("", result.getGamePlayer());
+	}
+
+	/**
+	 * Run the GameSaveInfo(String,String,String,int,int) constructor test.
+	 *
+	 * @throws Exception
+	 */
+	@Test
+	public void testGameSaveInfo_3()
+		throws Exception {
+		String saveName = "";
+		String gameName = "";
+		String gamePlayer = "";
+		int id = 1;
+		int gameScore = 1;
+
+		GameSaveInfo result = new GameSaveInfo(saveName, gameName, gamePlayer, id, gameScore);
+
+		assertNotNull(result);
+		assertEquals(1, result.getId());
+		assertEquals(0, result.getRank());
+		assertEquals(1, result.getGameScore());
+		assertEquals("", result.getSaveName());
+		assertEquals("", result.getGameName());
+		assertEquals(null, result.getGameData());
+		assertEquals("", result.getGamePlayer());
+	}
+
+	/**
+	 * Run the GameSaveInfo(String,String,String,int,int,String) constructor test.
+	 *
+	 * @throws Exception
+	 */
+	@Test
+	public void testGameSaveInfo_4()
+		throws Exception {
+		String saveName = "";
+		String gameName = "";
+		String gamePlayer = "";
+		int gameScore = 1;
+		int rank = 1;
+		String gameData = "";
+
+		GameSaveInfo result = new GameSaveInfo(saveName, gameName, gamePlayer, gameScore, rank, gameData);
+
+		assertNotNull(result);
+		assertEquals(0, result.getId());
+		assertEquals(1, result.getRank());
+		assertEquals(1, result.getGameScore());
+		assertEquals("", result.getSaveName());
+		assertEquals("", result.getGameName());
+		assertEquals("", result.getGameData());
+		assertEquals("", result.getGamePlayer());
+	}
+
+	/**
+	 * Run the String getGameData() method test.
+	 *
+	 * @throws Exception
+	 */
+	@Test
+	public void testGetGameData_1()
+		throws Exception {
+		GameSaveInfo fixture = new GameSaveInfo("", "", "", 1, 1, "");
+		fixture.setId(1);
+
+		String result = fixture.getGameData();
+
+		assertEquals("", result);
+	}
+
+	/**
+	 * Run the String getGameName() method test.
+	 *
+	 * @throws Exception
+	 */
+	@Test
+	public void testGetGameName_1()
+		throws Exception {
+		GameSaveInfo fixture = new GameSaveInfo("", "", "", 1, 1, "");
+		fixture.setId(1);
+
+		String result = fixture.getGameName();
+
+		assertEquals("", result);
+	}
+
+	/**
+	 * Perform pre-test initialization.
+	 *
+	 * @throws Exception
+	 *         if the initialization fails for some reason
+	 */
+	@Before
+	public void setUp()
+		throws Exception {
+		// add additional set up code here
+	}
+
+	/**
+	 * Perform post-test clean-up.
+	 *
+	 * @throws Exception
+	 *         if the clean-up fails for some reason
+	 */
+	@After
+	public void tearDown()
+		throws Exception {
+		// Add additional tear down code here
+	}
+
+	/**
+	 * Launch the test.
+	 *
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		new org.junit.runner.JUnitCore().run(GameSaveInfoTest.class);
+	}
+}
