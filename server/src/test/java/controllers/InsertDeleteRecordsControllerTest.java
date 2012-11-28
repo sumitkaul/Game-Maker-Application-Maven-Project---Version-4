1cc5d6e92a1ee1680df2fe27d9c676f841233616
harsh <harsh.savla@gmail.com>
2012-11-28 11:16:48 -0500

    http://tintin.cs.indiana.edu:8091/browse/FATWVTEN-98 Adding test for the "InsertDeleteRecordsController"

---
+++ server/src/test/java/controllers/InsertDeleteRecordsControllerTest.java
@@ -0,0 +1,50 @@
+package controllers;
+
+import junit.framework.Assert;
+
+import org.jboss.logging.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import com.google.gson.Gson;
+
+import db.DatabaseHandler;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(DatabaseHandler.class)
+public class InsertDeleteRecordsControllerTest {
+
+	InsertDeleteRecordsController controller;
+
+	@Before
+	public void setUp() {
+		controller = new InsertDeleteRecordsController();
+	}
+
+	@Test
+	public void testDeleteHostedGameBaseRecord() {
+
+		PowerMockito.mockStatic(DatabaseHandler.class);
+		Gson gson = new Gson();
+
+		try {
+			int returnedValue = 1;
+			PowerMockito.when(DatabaseHandler.class, DatabaseHandler.ExecuteQuery((String) Matchers.any())).thenReturn(
+					returnedValue);
+			String value = gson.toJson(false);
+			String sqlQuery = "delete from HostedGameBases where hostname=\"m\"";
+			String actualVal = controller.deleteHostedGameBaseRecord(sqlQuery);
+			if (value.equals(actualVal))
+				Assert.assertTrue(true);
+			else
+				Assert.assertTrue(false);
+		} catch (Exception e) {
+			Logger.getLogger(InsertDeleteRecordsControllerTest.class).debug(e.getMessage());
+		}
+	}
+}
