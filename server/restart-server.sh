a24c148c650249bee0bfc83ae0b156ae0cf6c136
Hari Prasad <hhariprsd@gmail.com>
2012-11-27 22:19:06 -0500

    updating restart-server.sh to recursively kill all the cargo:ru process...

--- server/restart-server.sh
+++ server/restart-server.sh
@@ -10,8 +10,17 @@ fi
 
 server_processid=`ps aux | awk '/8097/ {print $2}' | grep -v awk | head -1`;
 kill -9 $server_processid;
-cargo_processid=`ps aux | awk '/cargo:run/ {print $2}' | grep -v awk | head -1`;
-kill -9 $cargo_processid;
+
+count=`ps aux | awk '/cargo:run/ {print $2}' | wc -l`;
+while [ $count != 1 ]
+do
+        process_id=`ps aux | awk '/cargo:run/ {print $2}' | tail -1`;
+        kill -9 $process_id;
+        count=`ps aux | awk '/cargo:run/ {print $2}' | wc -l`;
+done
+
+# cargo_processid=`ps aux | awk '/cargo:run/ {print $2}' | grep -v awk | head -1`;
+# kill -9 $cargo_processid;
 
 /u/kspace/local/maven/bin/mvn -f /u/kspace/www/fluency/student-files/fall2012/a10/team-all/server/pom.xml clean install > $LOGFILE 2>&1
 /u/kspace/local/maven/bin/mvn -f /u/kspace/www/fluency/student-files/fall2012/a10/team-all/server/pom.xml cargo:run >> $LOGFILE 2>&1 &
