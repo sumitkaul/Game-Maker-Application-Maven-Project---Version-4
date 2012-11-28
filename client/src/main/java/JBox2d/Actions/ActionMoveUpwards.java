3038c00e995d6cac6c260a9fb305b237a021e0ce
ralekar <ralekar@indiana.edu>
2012-11-27 20:24:08 -0500

    http://tintin.cs.indiana.edu:8091/browse/FATWVTEN-125 Adding Move Upwards in JBox Action Packages

---
+++ client/src/main/java/JBox2d/Actions/ActionMoveUpwards.java
@@ -0,0 +1,24 @@
+package JBox2d.Actions;
+
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.Body;
+
+public class ActionMoveUpwards implements JBoxGameAction{
+    
+    public ActionMoveUpwards()
+    {
+              
+    }
+
+  @Override
+  public void doAction(Body body) {
+       
+        Vec2 bodyPosition=body.getPosition();
+        bodyPosition.y-=1f;
+        body.setLinearVelocity(bodyPosition);
+       }
+    
+    
+    
+    
+}
