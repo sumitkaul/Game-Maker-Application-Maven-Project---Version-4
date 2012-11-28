83b34faa10fd02d2fea10b63cd2f1a20440c64c7
Hari Prasad <hhariprsd@gmail.com>
2012-11-27 22:42:12 -0500

    test cases need a server instances before mvn clean install

--- server/restart-server.sh
+++ server/restart-server.sh
@@ -13,7 +13,7 @@ server_processid=`ps aux | awk '/8097/ {print $2}' | grep -v awk | head -1`;
 kill -9 $server_processid;
 
 count=`ps aux | awk '/cargo:run/ {print $2}' | wc -l`;
-while [ $count != 1 ]
+while [ $count != 2 ]
 do
         process_id=`ps aux | awk '/cargo:run/ {print $2}' | tail -1`;
         kill -9 $process_id;
@@ -23,6 +23,7 @@ done
 # cargo_processid=`ps aux | awk '/cargo:run/ {print $2}' | grep -v awk | head -1`;
 # kill -9 $cargo_processid;
 
+/u/kspace/local/maven/bin/mvn -f /u/kspace/www/fluency/student-files/fall2012/a10/team-all/server/pom.xml cargo:run >> $LOGFILE 2>&1 &
 
 /u/kspace/local/maven/bin/mvn -f /u/kspace/www/fluency/student-files/fall2012/a10/team-all/server/pom.xml clean install > $LOGFILE 2>&1
 /u/kspace/local/maven/bin/mvn -f /u/kspace/www/fluency/student-files/fall2012/a10/team-all/server/pom.xml cargo:run >> $LOGFILE 2>&1 &
