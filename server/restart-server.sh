61411a87407205520309ebfb71135f17dbd73789
Hari Prasad <hhariprsd@gmail.com>
2012-11-28 01:15:48 -0500

    updating restart-server.sh

--- server/restart-server.sh
+++ server/restart-server.sh
@@ -9,13 +9,13 @@ then
   kill -9 $PID
 fi
 
-server_processid=`ps aux | awk '/8097/ {print $2}' | grep -v awk | head -1`;
+server_processid=`ps aux --sort=start_time | awk '/8097/ {print $2}' | grep -v awk | head -1`;
 kill -9 $server_processid;
 
 count=`ps aux | awk '/cargo:run/ {print $2}' | wc -l`;
-while [ $count != 2 ]
+while [ $count != 1 ]
 do
-        process_id=`ps aux | awk '/cargo:run/ {print $2}' | tail -1`;
+        process_id=`ps aux --sort=lstart | awk '/cargo:run/ {print $2}' | tail -1`;
         kill -9 $process_id;
         count=`ps aux | awk '/cargo:run/ {print $2}' | wc -l`;
 done
@@ -25,7 +25,7 @@ done
 
 /u/kspace/local/maven/bin/mvn -f /u/kspace/www/fluency/student-files/fall2012/a10/team-all/server/pom.xml cargo:run >> $LOGFILE 2>&1 &
 
-/u/kspace/local/maven/bin/mvn -f /u/kspace/www/fluency/student-files/fall2012/a10/team-all/server/pom.xml clean install > $LOGFILE 2>&1
-/u/kspace/local/maven/bin/mvn -f /u/kspace/www/fluency/student-files/fall2012/a10/team-all/server/pom.xml cargo:run >> $LOGFILE 2>&1 &
+# /u/kspace/local/maven/bin/mvn -f /u/kspace/www/fluency/student-files/fall2012/a10/team-all/server/pom.xml clean install > $LOGFILE 2>&1
+# /u/kspace/local/maven/bin/mvn -f /u/kspace/www/fluency/student-files/fall2012/a10/team-all/server/pom.xml cargo:run >> $LOGFILE 2>&1 &
 pid=$!
 echo $pid > $PIDFILE
