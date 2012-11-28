804a5fd3b849fe341c260369ca7c2eb20292d55a
harsh <harsh.savla@gmail.com>
2012-11-28 07:29:50 -0500

    http://tintin.cs.indiana.edu:8091/browse/FATWVTEN-99 adding test for ListAllGamesHost controller

---
+++ server/src/test/java/controllers/ListAllHostedGamesTest.java
@@ -0,0 +1,48 @@
+package controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.jboss.logging.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Matchers;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import com.google.gson.Gson;
+
+import db.DatabaseHandler;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ DatabaseHandler.class })
+public class ListAllHostedGamesTest {
+
+	ListAllHostedGames listAllHostedGames = new ListAllHostedGames();
+
+	@Test
+	public void testLoadHostGames() {
+		PowerMockito.mockStatic(DatabaseHandler.class);
+		List<String> listGames = new ArrayList<String>();
+
+		listGames.add("pacman");
+		listGames.add("asteroids");
+
+		try {
+
+			PowerMockito.when(DatabaseHandler.class, "Query", Matchers.any()).thenReturn(listGames);
+			Gson gson = new Gson();
+			String json = gson.toJson(listGames);
+			String actualResult = listAllHostedGames.loadHostGames();
+			if (actualResult.equals(json)) {
+				Assert.assertTrue(true);
+			} else
+				Assert.assertTrue(false);
+		} catch (Exception e) {
+			Logger.getLogger(ListAllHostedGamesTest.class).debug(e.getMessage());
+		}
+	}
+}
