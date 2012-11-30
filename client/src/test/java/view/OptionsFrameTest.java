8f1fb00d751646fd09d1cebf290b95ca9e560b6b
Nikhil <Nikhil@Nikhil-PC>
2012-11-30 14:37:27 -0500

    JIRA issue: [A10 FATWVTEN-159] Problem: the options frame class may return a null pointer if erronous changes are made to the singleton instance creation.

---
+++ client/src/test/java/view/OptionsFrameTest.java
@@ -0,0 +1,44 @@
+
+package view;
+
+import java.awt.event.ActionEvent;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class OptionsFrameTest {
+    
+    public OptionsFrameTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    } 
+
+    @Test
+    public void testGetOptionFrame() {
+        System.out.println("getOptionFrame");
+//        OptionsFrame expResult = null;
+        OptionsFrame result = OptionsFrame.getOptionFrame();
+        assertNotNull(result);        
+    }
+
+    @Test
+    public void testSetVisible() {
+        
+        boolean visibility = false;
+        OptionsFrame instance = OptionsFrame.getOptionFrame();
+        instance.setVisible(visibility);        
+    }
+}
