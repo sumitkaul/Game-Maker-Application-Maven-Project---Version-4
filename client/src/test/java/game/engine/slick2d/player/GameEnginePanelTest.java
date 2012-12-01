08cc7d9eda95a5e08e3d1cf1d861404f3b943012
sumit <sumikaul@indiana.edu>
2012-11-30 22:01:32 -0500

    Changes for http://tintin.cs.indiana.edu:8091/browse/FATWVTEN-166

---
+++ client/src/test/java/game/engine/slick2d/player/GameEnginePanelTest.java
@@ -0,0 +1,55 @@
+package game.engine.slick2d.player;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.newdawn.slick.CanvasGameContainer;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class GameEnginePanelTest {
+
+	GameEnginePanel gameEnginePanel,gameEnginePanelMock;
+	CanvasGameContainer canvasGameContainer,expectedApp,actualApp;
+	GameEngineController gameEngineController,expectedGameEngineController,actualGameEngineController;
+	
+	
+	@Before
+	public void setUp() throws Exception {
+		gameEnginePanel= new GameEnginePanel();
+		gameEnginePanelMock= mock(GameEnginePanel.class);
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		gameEnginePanel=null;
+	}
+
+	@Test
+	public void testGameEnginePanel() {
+		
+	}
+
+	@Test
+	public void testSetApp() {
+		gameEnginePanel.setApp(canvasGameContainer);
+		actualApp= gameEnginePanel.getApp();
+		expectedApp= canvasGameContainer;
+		assertEquals(expectedApp,actualApp);
+	}
+
+	@Test
+	public void testGetGame() {
+		gameEnginePanel.setGame(gameEngineController);
+		actualGameEngineController= gameEnginePanel.getGame();
+		expectedGameEngineController= gameEngineController;
+		assertEquals(expectedGameEngineController,actualGameEngineController);
+		
+	}
+
+}
