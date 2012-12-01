a3611c4ae8cd0f970cca8f1e14702fa66272f5ff
mjaffee <mjaffee@indiana.edu>
2012-12-01 15:15:22 -0500

    add test for registerUser in LoginController http://tintin.cs.indiana.edu:8091/browse/FATWVTEN-99#comment-10725

--- server/src/test/java/controllers/LoginControllerTest.java
+++ server/src/test/java/controllers/LoginControllerTest.java
@@ -6,6 +6,7 @@ import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Matchers;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -29,7 +30,7 @@ public class LoginControllerTest {
 		PowerMockito.mockStatic(DatabaseHandler.class);
 
 		try {
-			ArrayList retValue = new ArrayList();
+			ArrayList<User> retValue = new ArrayList<User>();
 			retValue.add(new User("asdf", "asdf", "1"));
 			PowerMockito.when(DatabaseHandler.class,
 					DatabaseHandler.loginQuery("asdf", "asdf")).thenReturn(
@@ -37,7 +38,7 @@ public class LoginControllerTest {
 			String ret = loginController.login("asdf", "asdf");
 			assert (ret == "true");
 
-			ArrayList secondRetValue = new ArrayList();
+			ArrayList<User> secondRetValue = new ArrayList<User>();
 			PowerMockito.when(DatabaseHandler.class,
 					DatabaseHandler.loginQuery("salkdf", "sdfsdfsdf"))
 					.thenReturn(secondRetValue);
@@ -47,7 +48,19 @@ public class LoginControllerTest {
 		} catch (Exception e) {
 			LOG.error(e);
 		}
-
+	}
+	
+	@Test
+	public void testRegister(){
+		PowerMockito.mockStatic(DatabaseHandler.class);
+		try {
+			PowerMockito.when(DatabaseHandler.class, 
+					DatabaseHandler.save(new Object())).thenReturn(true);
+			String ret = loginController.register("zzsfp", "zzqfp");
+			assert(ret=="true");
+		} catch(Exception e) {
+			LOG.error(e);
+		}
 	}
 
 }
