32390c842438ef1d27fe4d1544d10b81f4ca77ed
harsh <harsh.savla@gmail.com>
2012-12-01 07:34:15 -0500

    http://tintin.cs.indiana.edu:8091/browse/FATWVTEN-165 removing sysout statement and cleaning up code

--- client/src/test/java/jBox2d/main/JBoxObjectListTest.java
+++ client/src/test/java/jBox2d/main/JBoxObjectListTest.java
@@ -3,83 +3,69 @@
  * and open the template in the editor.
  */
 package jBox2d.main;
-import org.mockito.invocation.InvocationOnMock;
-import java.util.LinkedHashMap;
-import org.jbox2d.dynamics.Body;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.inOrder;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.doCallRealMethod;
 
-import org.mockito.InOrder;
-import org.mockito.stubbing.Answer;
-import org.mockito.Mockito;
-import JBox2d.main.JBoxObjectList;
-import JBox2d.main.JBoxSpriteModel;
+import static org.mockito.Mockito.doCallRealMethod;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.inOrder;
+
+import java.util.LinkedHashMap;
 import java.util.Map;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.mockito.InOrder;
+import org.mockito.Mockito;
 
+import JBox2d.main.JBoxObjectList;
+import JBox2d.main.JBoxSpriteModel;
 
 public class JBoxObjectListTest {
-    private static JBoxObjectList mock ;
-    public JBoxObjectListTest() {
-    }
+	private static JBoxObjectList mock;
 
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-        
-    }
+	public JBoxObjectListTest() {
+	}
 
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-       
-    }
-    
-    @Before
-    public void setUp() {
-         mock = Mockito.mock(JBoxObjectList.class);
-    }
-    
-    @After
-    public void tearDown() {
-     mock=null;
-    }
+	@BeforeClass
+	public static void setUpClass() throws Exception {
 
-    /**
-     * Test of registerSpriteModel method, of class JBoxObjectList.
-     */
-    @Test
-    public void testRegisterSpriteModel() {
-        System.out.println("registerSpriteModel");
-        JBoxObjectList mock2=Mockito.mock(JBoxObjectList.class);
-        JBoxSpriteModel sprite1 = Mockito.mock(JBoxSpriteModel.class);
-        JBoxSpriteModel sprite2=Mockito.mock(JBoxSpriteModel.class);
-        mock.registerSpriteModel(sprite1);
-       mock2.registerSpriteModel(sprite2);
-       InOrder inOrder= inOrder(mock,mock2);
-        inOrder.verify(mock).registerSpriteModel(sprite1);
-        inOrder.verify(mock2).registerSpriteModel(sprite2);
-        
-      
-    }
+	}
 
-    /**
-     * Test of getListJBoxSpriteModel method, of class JBoxObjectList.
-     */
-    @Test
-    public void testSetListJBoxSpriteModel() throws Exception{
-        Map mocker=Mockito.mock(LinkedHashMap.class); 
-       doThrow(new RuntimeException()).when(mock).setListJBoxSpriteModel(mocker);
-       doCallRealMethod().when(mock).setListJBoxSpriteModel(mocker);
-       
-    }
-    
+	@AfterClass
+	public static void tearDownClass() throws Exception {
 
-   
+	}
+
+	@Before
+	public void setUp() {
+		mock = Mockito.mock(JBoxObjectList.class);
+	}
+
+	@After
+	public void tearDown() {
+		mock = null;
+	}
+
+	/** Test of registerSpriteModel method, of class JBoxObjectList. */
+	@Test
+	public void testRegisterSpriteModel() {
+		JBoxObjectList mock2 = Mockito.mock(JBoxObjectList.class);
+		JBoxSpriteModel sprite1 = Mockito.mock(JBoxSpriteModel.class);
+		JBoxSpriteModel sprite2 = Mockito.mock(JBoxSpriteModel.class);
+		mock.registerSpriteModel(sprite1);
+		mock2.registerSpriteModel(sprite2);
+		InOrder inOrder = inOrder(mock, mock2);
+		inOrder.verify(mock).registerSpriteModel(sprite1);
+		inOrder.verify(mock2).registerSpriteModel(sprite2);
+	}
+
+	/** Test of getListJBoxSpriteModel method, of class JBoxObjectList. */
+	@Test
+	public void testSetListJBoxSpriteModel() throws Exception {
+		Map mocker = Mockito.mock(LinkedHashMap.class);
+		doThrow(new RuntimeException()).when(mock).setListJBoxSpriteModel(mocker);
+		doCallRealMethod().when(mock).setListJBoxSpriteModel(mocker);
+	}
 }
