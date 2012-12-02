8abd7c65c20db77f129966308dc2e4ea55f553b2
Ridhima Mahajan <ridmahaj@indiana.edu>
2012-12-02 02:24:36 -0500

    fixing the ProtocolTest : JIRA FATWVTEN-171

--- client/src/test/java/multiplayer/ProtocolTest.java
+++ client/src/test/java/multiplayer/ProtocolTest.java
@@ -26,8 +26,12 @@ public class ProtocolTest {
 		SpriteModel model = new SpriteModel(10.0, 10.0, 5.0, 5.0, 20, 20, "", "1", 1);
 		model.setId("1");
 		SpriteList.getInstance().addSprite(model);
-		HashMap<GameAction, String> map = new HashMap<GameAction, String>();
-		map.put(new ActionChangeSpeed(9.0, 9.0), "1");
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		//map.put(new ActionChangeSpeed(9.0, 9.0), "1");
+		map.put("action",new ActionChangeSpeed(9.0, 9.0) );
+		map.put("speedX", 1.0);
+		map.put("speedY", 2.0);
+		map.put("id", "1");
 		protocol.setMultiplayerAction(map);
 		Assert.assertEquals(9.0, model.getSpeedX(), 0.0);
 	}
